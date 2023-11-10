7080c6827b3436a6726580d63e573ca135fe2174
Yeonwu Oh <yeonwu319@gmail.com>
2023-11-11 06:36:56 +0900

    최최최종

--- analysis/app.py
+++ analysis/app.py
@@ -6,14 +6,14 @@ import time
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from mz_power_calc import mz_test_everything
-# from mz_power_ai import mz_classify
+from mz_power_ai import mz_classify
 
 load_dotenv()
 SERVER_URL = os.getenv('SERVER_URL')
 
 def predict(predict_sentence):
 	result_power = mz_test_everything.mz_power(predict_sentence)
-	result_class = 1 #mz_classify.predict(predict_sentence)
+	result_class = mz_classify.predict(predict_sentence)
 
 	result_power_mean = sum(result_power)/len(result_power)
 
@@ -56,12 +56,12 @@ def analize():
 if __name__ == '__main__':
 	work = None
 	while True:
-		# try:
-		time.sleep(3)
-		print('Fetch Work.')
-		result = analize()
-		print('Analysis Result: ', result)
-		if result:
-			requests.post(SERVER_URL, json=result)
-		# except Exception as e:
-		# 	print(e)
+		try:
+			time.sleep(3)
+			print('Fetch Work.')
+			result = analize()
+			print('Analysis Result: ', result)
+			if result:
+				requests.post(SERVER_URL, json=result)
+		except Exception as e:
+			print(e)
