1507528a935c3f29c2bc377c83b88d9e1784afad
Yeonwu Oh <yeonwu319@gmail.com>
2023-11-11 05:37:59 +0900

    AI 모델

---
+++ mz_power_ai/install.sh
@@ -0,0 +1,12 @@
+pip install mxnet
+pip install gluonnlp==0.9.1
+pip install sentencepiece
+pip install transformers
+pip install torch
+pip install selenium
+pip install bs4
+pip install webdriver_manager
+pip install webdrivermanager
+pip install BeautifulSoup
+pip install 'git+https://github.com/SKTBrain/KoBERT.git#egg=kobert_tokenizer&subdirectory=kobert_hf'
+pip install openpyxl
